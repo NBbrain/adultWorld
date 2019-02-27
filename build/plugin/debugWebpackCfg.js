bb1e668f46f4f67bee357f684c7007bc4b9e95c5
mengyue <mengyue@hujiang.com>
2019-02-27 11:21:21 +0800

    webpack plugin

---
+++ build/plugin/debugWebpackCfg.js
@@ -0,0 +1,18 @@
+const pluginName = 'debugWebpackConfigPlugin';
+module.exports = class debugWebpackConfigPlugin {
+  constructor(options){
+
+  }
+  apply(compiler){
+    compiler.hooks.compilation.tap(pluginName, (compilation)=>{
+      compilation.hooks.succeedModule.tap(pluginName, (module)=>{
+        console.log(module);
+        console.log('============================');
+      })
+      compilation.hooks.succeedModule.tap(pluginName, (module)=>{
+        console.log(module);
+        console.log('失败============================');
+      })
+    })
+  }
+}
